04febbf37a79af03725165b183ac15db5cec8728
Kyle <huangkyle110@gmail.com>
2021-01-21 09:24:00 -0800

    Ignore .idea files

--- knapsack_dp.py
+++ knapsack_dp.py
@@ -1,13 +1,2 @@
 # Dynamic programming solution for knapsack problem
 # By: Kyle Huang
-
-# Function to solve knapsack problem
-def knapsack(W, wt, val, n):
-    
-
-# Driver function
-if __name__=="__main__":
-    val = [40, 50, 80]
-    wt = [5, 10, 15]
-    W = 20
-    n = len(val)
--- knapsack_recursive.py
+++ knapsack_recursive.py
@@ -1,2 +1,19 @@
 # Recursive solution for Knapsack problem
 # By: Kyle Huang
+#
+#           { 0,                                         n <= 0
+# F(n, W) = { F(n - 1, W),                               Wn > W
+#           { max(F(n - 1, W), valn + F(n - 1, W - Wn)), Wn <= W
+#
+
+
+# Function to solve knapsack problem
+def knapsack(W, wt, val, n):
+
+
+# Driver function
+if __name__=="__main__":
+    val = [40, 50, 80]
+    wt = [5, 10, 15]
+    W = 20
+    n = len(val)
